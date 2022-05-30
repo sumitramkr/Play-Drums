818f3c84ce4eb96358c05ad5420a3cd4620f293e
sumitramkr <sumitramkr77e@gmail.com>
2022-05-30 23:56:48 +0530

    added sound

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; ++i) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {alert("I am clicked");});
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+    });
 }
+
